493b4f1fdd10df6ce0e871978f92aa856e98c307
burakkaanozbek <burak.ozbek@binalyze.com>
2022-08-23 22:07:22 +0300

    refactor: remove inmem router

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -4,21 +4,16 @@ import (
 	"net/http"
 
 	"github.com/ozbekburak/cleanarch-mongo-inmem/adapter/controller"
-	"github.com/ozbekburak/cleanarch-mongo-inmem/pkg/db"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Initialize function initializes the router
-func Initialize(conn *mongo.Client, inMem *db.InMemoryDB) *http.ServeMux {
+func Initialize(conn *mongo.Client) *http.ServeMux {
 	recordController := controller.NewRecordController(conn)
-	inMemoryController := controller.NewInMemoryController(inMem)
 
 	router := http.NewServeMux()
 
 	router.Handle("/records", recordController.Filter())
-	// In-memory DB handlers go here
-	router.Handle("/in-memory/set", inMemoryController.Store())
-	router.Handle("/in-memory", inMemoryController.Get())
 
 	return router
 }
