d303d38a301bfff30166f29c9caf18221b2f485b
burakkaanozbek <burak.ozbek@binalyze.com>
2022-08-24 22:07:09 +0300

    refactor: update error messages

--- domain/errors.go
+++ domain/errors.go
@@ -3,7 +3,5 @@ package domain
 import "errors"
 
 var (
-	ErrMaxCountLessThanMin  = errors.New("Min count must be less than max count")
-	ErrStartDateLessThanEnd = errors.New("Start date must be before end date")
-	ErrEmptyKey             = errors.New("Key cannot be empty")
+	ErrUserNotExist = errors.New("User not exist")
 )
