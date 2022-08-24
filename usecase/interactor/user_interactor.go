318bd765577d61942af91dcb1518b16ca9afbf51
burakkaanozbek <burak.ozbek@binalyze.com>
2022-08-24 22:07:01 +0300

    refactor: update interactor with user

---
+++ usecase/interactor/user_interactor.go
@@ -0,0 +1,17 @@
+package interactor
+
+import (
+	"github.com/ozbekburak/cleanarch-mongo-inmem/domain"
+	"github.com/ozbekburak/cleanarch-mongo-inmem/usecase/repository"
+)
+
+type UserInteractor struct {
+	UserRepository repository.UserRepository
+}
+
+func (ui *UserInteractor) Create(u domain.User) (int64, error) {
+	var id int64
+
+	id, err := ui.UserRepository.Create(u)
+	return id, err
+}
